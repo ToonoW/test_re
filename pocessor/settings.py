591a6434ccc788e7813828012b3d07458f95d74e
Jam <jamhuang@gizwits.com>
2016-03-21 17:46:47 +0800

    fix

--- pocessor/settings.py
+++ pocessor/settings.py
@@ -9,11 +9,14 @@ import dj_database_url
 
 
 # M2M settings
+M2M_MQ_URL = env('M2M_MQ_URL', 'amqp://guest:guest@m2mprod.gwdev.com:5672/mqtt')
 M2M_HOST = env('M2M_HOST', 'm2mprod.gwdev.com')
 M2M_PORT = env('M2M_PORT', 5672)
 VHOST = env('VHOST', '/')
 M2M_USER = env('M2M_USER', 'guest')
 M2M_PWD = env('M2M_PWD', 'guest')
+
+EXCHANGE = 'amq.topic'
 
 # databases settings
 MONGO_DATABASES = env("MONGO_GIZWITS_DATA", "mongodb://localhost:27017/gizwits_data")
@@ -29,12 +32,22 @@ LOGGING = {
             'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
             'datefmt': "%d/%b/%Y %H:%M:%S"
         },
+        'biz_log': {
+            'format': ' %(message)s'
+        }
     },
     'handlers': {
         "console": env("LOG_CONSOLE", {"level": "INFO", "class": "logging.StreamHandler", "formatter": "standard"}),
         #"graylog": env("LOG_GRAYLOG", {"level": "INFO", "class": "graypy.GELFRabbitHandler", "url": "amqp://guest:guest@localhost:5672/%2f"}),
     },
     'loggers': {
+        'file': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'INFO',
+            'when': 'D',
+            'filename': '/mnt/workspace/gw_re_pocessor/gizwits_data.log',
+            'formatter': 'standard',
+        },
         'pocessor': {
             'handlers': ['console'],
             'propagate': True,
