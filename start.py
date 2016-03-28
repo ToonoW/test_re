5f179f4a748239dc9ab8be3b9c07284c5c2a2c0c
Jam <jamhuang@gizwits.com>
2016-03-28 18:51:41 +0800

    fix

--- start.py
+++ start.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""
+Usage:
+  start.py [options] <queue>
+
+Options:
+  -h --help          Show this screen.
+  --version          Show version.
+  --speed=<kn>
+"""
 
 from gevent import monkey
 monkey.patch_all()
@@ -11,12 +20,15 @@ import sys, os
 sys.path.append(os.environ.get('SYS_PATH', '.'))
 
 import gevent
+from docopt import docopt
 
 #from re_processor import settings
 from re_processor.container import get_container
 
 
 if '__main__' == __name__:
+    args = docopt(__doc__, version='RulesEngine Processor 0.1.0')
+    print args
     queue = 'all'
     gevent.joinall([
         gevent.spawn(get_container(queue, container_type='main').begin),
