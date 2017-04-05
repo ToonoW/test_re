eb46f6c46d8ce21705aa0293730973838ee82aee
Jam <jamhuang@gizwits.com>
2017-04-05 18:32:12 +0800

    fix

--- re_processor/main/__init__.py
+++ re_processor/main/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from dispatcher import MainDispatcher, ScheduleBufferConsumer, DeviceScheduleScanner, ProductScheduleScanner
+from dispatcher import MainDispatcher, DeviceScheduleScanner
 
-__all__ = ['MainDispatcher', 'ScheduleBufferConsumer', 'DeviceScheduleScanner', 'ProductScheduleScanner']
+__all__ = ['MainDispatcher', 'DeviceScheduleScanner']
